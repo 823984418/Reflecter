753de24b1f1d3c19deec986bba6a89b263ea546b
823984418 <823984418@qq.com>
2019-07-04 10:34:05 +0000

    Create Violence.java

---
+++ src/net/mock/reflecter/annotation/Violence.java
@@ -0,0 +1,10 @@
+package net.mock.reflecter.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.CONSTRUCTOR,ElementType.METHOD,ElementType.FIELD})
+public @interface Violence{
+	
+}
